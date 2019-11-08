129f7bd89a2e26e42968cde7e58cb4f50ca117bd
DESKTOP-3LKHHQC\Excellent <977506457@qq.com>
2019-11-08 14:34:19 +0800

    feat:首页icon列表总数

--- src/view/goods-nature/brandList.vue
+++ src/view/goods-nature/brandList.vue
@@ -249,6 +249,7 @@ export default {
       this.tableLoading = false
       if (res.data.code === 0) {
         console.log(res.data.content)
+        this.total = +res.data.content.total
         this.dataList = res.data.content.rows
       }
     },
