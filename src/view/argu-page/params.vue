fee34f4dfc4f73884f83b5d56f5b898164ada733
z1511676208 <1511676208@qq.com>
2019-11-14 11:48:22 +0800

    feat:git test

--- src/view/argu-page/params.vue
+++ src/view/argu-page/params.vue
@@ -17,7 +17,7 @@ export default {
     ]),
     close () {
       /**
-       * 如果是调用closeTag方法，普通的页面传入的对象参数只需要写name字段即可
+       * 如果是调用closeTag方法，普通的页面传入的对象参数只需要写name字段即可 000
        * 如果是动态路由和带参路由，需要传入query或params字段，用来区别关闭的是参数为多少的页面
        */
       this.closeTag({
