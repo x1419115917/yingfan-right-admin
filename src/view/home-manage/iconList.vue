93474ccb49d3f3d2f91f2d1e4a4abc438414ab85
z1511676208 <1511676208@qq.com>
2019-12-03 16:32:50 +0800

    fix：iconlist调整

--- src/view/home-manage/iconList.vue
+++ src/view/home-manage/iconList.vue
@@ -147,6 +147,14 @@ export default {
       tableLoading: false
     }
   },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      // 因为当钩子执行前，组件实例还没被创建
+      // vm 就是当前组件的实例相当于上面的 this，所以在 next 方法里你就可以把 vm 当 this 来用了。
+      vm.getPageList()
+      vm.getActivityList()
+    })
+  },
   components: {
     addIcons
   },
