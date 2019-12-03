c52512eb3f2985baefd4c2e7c89ea547cd76987a
z1511676208 <1511676208@qq.com>
2019-12-03 15:30:25 +0800

    feat：活动专题修改

--- src/view/thematic-activities/thematicList.vue
+++ src/view/thematic-activities/thematicList.vue
@@ -149,6 +149,13 @@ export default {
       tableLoading: false
     }
   },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      // 因为当钩子执行前，组件实例还没被创建
+      // vm 就是当前组件的实例相当于上面的 this，所以在 next 方法里你就可以把 vm 当 this 来用了。
+      vm.getPageList()
+    })
+  },
   methods: {
     async getPageList () {
       this.tableLoading = true
