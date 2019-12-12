90e484049c48f76b926d780674cefdf2b9bffc2a
z1511676208 <1511676208@qq.com>
2019-12-12 17:12:22 +0800

    feat：提交配置修改

--- vue.config.js
+++ vue.config.js
@@ -34,6 +34,26 @@ module.exports = {
       config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
     }
   },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          include: [
+            resolve('src'),
+            resolve('test'),
+            resolve('node_modules')
+          ],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  },
   // 设为false打包时不生成.map文件
   productionSourceMap: false
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
