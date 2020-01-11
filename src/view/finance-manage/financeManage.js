b04946d545b5606dfbb800c347ad51c1c4996496
guozhaobin <1419115917@qq.com>
2020-01-11 17:53:05 +0800

    feat:添加流水交易字段

--- src/view/finance-manage/financeManage.js
+++ src/view/finance-manage/financeManage.js
@@ -15,6 +15,27 @@ export function orderStatus () {
     }
   ]
 }
+/* 支付来源 */
+export function paySource () {
+  return [
+    {
+      value: '',
+      label: '全部'
+    },
+    {
+      value: 0,
+      label: '微信小程序支付'
+    },
+    {
+      value: 1,
+      label: '微信支付'
+    },
+    {
+      value: 2,
+      label: '支付宝支付'
+    }
+  ]
+}
 /* 提现状态 */
 export function cashoutStatus () {
   return [
--- src/view/finance-manage/payList.vue
+++ src/view/finance-manage/payList.vue
@@ -2,16 +2,44 @@
 <template>
   <div class="tradeList">
     <Card title="交易流水">
-      <Row>
+      <Row class="rowMargin">
+        <Col span="6">
+          <span>支付时间</span>
+            <DatePicker
+              type="daterange"
+              @on-change="selectDate"
+              v-model="date"
+              placeholder="年/月/日" ></DatePicker>
+        </Col>
+        <Col span="6">
+          <span>商户订单号</span>
+          <Input v-model="form.serialNumber" :style="{ width: inpWidth}" placeholder="请输入商户订单号" clearable />
+        </Col>
         <Col span="6">
           <span>订单号</span>
           <Input v-model="form.businessId" :style="{ width: inpWidth}" placeholder="请输入订单号" clearable />
         </Col>
-        <Col span="18">
+        <Col span="6">
           <span>支付状态</span>
           <Select v-model="form.status" :style="{ width: inpWidth}" clearable>
             <Option v-for="item in orderStatusOpts" :value="item.value" :key="item.value">{{ item.label }}</Option>
           </Select>
+        </Col>
+      </Row>
+      <Row>
+        <Col span="6">
+          <span>交易金额</span>
+          <Input v-model.number="form.payFeeBegin" :style="{ width: moneyInpWidth}" clearable />
+          <span class="line">-</span>
+          <Input v-model.number="form.payFeeEnd" :style="{ width: moneyInpWidth}" clearable @on-blur="checkRange" />
+        </Col>
+        <Col span="6">
+          <span>支付来源</span>
+          <Select v-model="form.paySource" :style="{ width: inpWidth}" clearable>
+            <Option v-for="item in paySourceOpts" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
+        </Col>
+        <Col span="6">
           <Button type="primary" style="margin: 0 10px" @click="search">查询</Button>
           <Button type="primary" @click="download">数据下载</Button>
         </Col>
@@ -19,42 +47,50 @@
     </Card>
     <div class="wrap">
       <Table :columns="columns" border :data="dataList" stripe>
-          <template slot-scope="{ row, index }" slot="timeEnd">
-            <span v-if="row.timeEnd">{{ $moment(row.timeEnd).format("YYYY-MM-DD HH:mm:ss")}}</span>
-            <span v-else>-</span>
-          </template>
-          <template slot-scope="{ row, index }" slot="status"><span>{{ returnStatus(row.status) }}</span></template>
-          <template slot-scope="{ row, index }" slot="paySource"><span>{{ returnPaySource(row.paySource) }}</span></template>
-          <template slot-scope="{ row, index }" slot="payFee">
-            <span v-if="row.payFee">¥{{ row.payFee }}</span>
-            <span v-else>-</span>
-          </template>
-        </Table>
-        <Page
-          :current="form.pageNum"
-          :page-size="form.pageSize"
-          :total="pageTotal"
-          show-total
-          show-elevator
-          show-sizer
-          @on-page-size-change="changePageSize"
-          @on-change="pageChange"/>
+        <template slot-scope="{ row, index }" slot="timeEnd">
+          <span v-if="row.timeEnd">{{ $moment(row.timeEnd).format("YYYY-MM-DD HH:mm:ss")}}</span>
+          <span v-else>-</span>
+        </template>
+        <template slot-scope="{ row, index }" slot="status"><span>{{ returnStatus(row.status) }}</span></template>
+        <template slot-scope="{ row, index }" slot="paySource"><span>{{ returnPaySource(row.paySource) }}</span></template>
+        <template slot-scope="{ row, index }" slot="payFee">
+          <span v-if="row.payFee">¥{{ row.payFee }}</span>
+          <span v-else>-</span>
+        </template>
+      </Table>
+      <Page
+        :current="form.pageNum"
+        :page-size="form.pageSize"
+        :total="pageTotal"
+        show-total
+        show-elevator
+        show-sizer
+        @on-page-size-change="changePageSize"
+        @on-change="pageChange"/>
       </div>
   </div>
 </template>
 <script>
 import { doPayList } from '@/api/finance'
-import { orderStatus } from './financeManage'
+import { orderStatus, paySource } from './financeManage'
 export default {
   name: 'payList',
   data () {
     return {
       inpWidth: '162px',
+      moneyInpWidth: '80px',
       pageTotal: null,
+      date: [], // 选择日期
       dataList: [], // 订单列表
       form: {
         FLAG: 1,
+        payFeeBegin: null,
+        payFeeEnd: null,
+        paySource: null,
         businessId: null,
+        serialNumber: null,
+        timeEndBegin: null,
+        timeEndEnd: null,
         status: null,
         pageIndex: 1,
         pageSize: 10
@@ -107,6 +143,18 @@ export default {
         case 1 : return '交易成功'
       }
     },
+    selectDate () {
+      this.form.timeEndBegin = this.$moment(new Date(this.date[0])).format('YYYY-MM-DD')
+      this.form.timeEndEnd = this.$moment(new Date(this.date[1])).format('YYYY-MM-DD')
+    },
+    checkRange () {
+      if (this.form.payFeeBegin > this.form.payFeeEnd) {
+        this.$Message.warning('输入的金额起始值需小于末尾值')
+        this.form.payFeeBegin = null
+        this.form.payFeeEnd = null
+        return false
+      }
+    },
     // 查询订单
     search () {
       this.form.pageIndex = 1
@@ -125,6 +173,10 @@ export default {
     },
     async getPayList () {
       let data = this.form
+      if (this.form.timeEndBegin === 'Invalid date') {
+        this.form.timeEndBegin = ''
+        this.form.timeEndEnd = ''
+      }
       let res = await doPayList(data)
       if (res.data.code === 0) {
         this.dataList = res.data.content.rows
@@ -137,6 +189,9 @@ export default {
   computed: {
     orderStatusOpts () {
       return orderStatus()
+    },
+    paySourceOpts () {
+      return paySource()
     }
   },
   created () {
@@ -151,10 +206,16 @@ export default {
     padding: 14px;
     background-color: #fff;
   }
+  .rowMargin {
+    margin-bottom: 20px;
+  }
   .ivu-card-body {
     .ivu-col {
       span {
         margin-right: 6px;
+      }
+      .line {
+        margin: 0 10px;
       }
     }
   }
